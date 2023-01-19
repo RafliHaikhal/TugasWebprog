fc1dcac363341d7abcb2f09880189cc6dc4454d3
RafliHaikhal <rafli.haikhal@gmail.com>
2023-01-19 23:19:58 +0700

    test

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="http://127.0.0.1:8000/css/style.css">
+    <link rel="shortcut icon" href="http://127.0.0.1:8000/assets/joystick.png" type="image/x-icon">
+    <title>@yield('title')</title>
+</head>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="">
+      <div class="navbar-row row">
+        <div class="navbar-logo col">
+          <img src="http://127.0.0.1:8000/assets/joystick.png" class="navbar-logo-picture" alt="...">
+        </div>
+        <div class="navbar-title col">
+          <h3 class="navbar-title-text">Dreamyy Gaming Store</h3>
+        </div>
+      </div>
+    </a>
+  </div>
+</nav>
